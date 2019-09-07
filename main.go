b38df32649dfa5e9fc0621922cde0e2288b13b2e
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-09-07 11:04:56 -0700

    test: test env vars

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"os"
+	"fmt"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
+		Body:       fmt.Printf("%s is %s. years old\n", os.Getenv("NAME"), os.Getenv("AGE")),
 	}, nil
 }
 
